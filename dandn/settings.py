2a331da5aa51713c7279fcd8dc6afb59d2081e37
Osama Mhd <osamamhd82@gmail.com>
2021-03-04 12:26:00 +0200

    update static dirs.

--- dandn/settings.py
+++ dandn/settings.py
@@ -125,11 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/frontend/static/'
 
 # default static files settings for PythonAnywhere.
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
 MEDIA_ROOT = '/home/dreamsandnightmares/dandn/media'
 MEDIA_URL = '/media/'
-STATIC_ROOT = '/home/dreamsandnightmares/dandn/static'
-STATIC_URL = '/static/'
+STATIC_ROOT = '/home/dreamsandnightmares/dandn/frontend/static'
+STATIC_URL = '/frontend/static/'
