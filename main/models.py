8239f69ac7c90dd5f02cf84dbe2e7c8600f5a4e8
Osama Mhd <osamamhd82@gmail.com>
2021-03-04 17:21:03 +0200

    create story model.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
+from django.utils import timezone
 from django.db import models
+from autoslug import AutoSlugField
 
-# Create your models here.
+class Story(models.Model):
+    title = models.CharField(max_length=200)
+    slug = AutoSlugField(populate_from='title')
+    body = models.TextField(blank=True)
+    pub_date = models.DateField(default=timezone.now().strftime("%m-%d-%y"))
+    points = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["-points"]
+
+    def __str__(self):
+        return self.title
