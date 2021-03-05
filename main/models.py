e4b21f2b0f9c33700dfacdcf6134bc9fbad38a2b
Osama Mhd <osamamhd82@gmail.com>
2021-03-05 16:12:42 +0200

    type of the story 'Dream' or 'Nightmare'

--- main/models.py
+++ main/models.py
@@ -2,12 +2,18 @@ from django.utils import timezone
 from django.db import models
 from autoslug import AutoSlugField
 
+TYPES = (
+    ('dream', 'Dream'),
+    ('nightmare', 'Nightmare')
+)
+
 class Story(models.Model):
     title = models.CharField(max_length=200)
     slug = AutoSlugField(populate_from='title')
     body = models.TextField(blank=True)
     pub_date = models.DateField(default=timezone.now().strftime("%m-%d-%y"))
     points = models.IntegerField(default=0)
+    type_of_story = models.CharField(max_length=20, choices=TYPES, default='dream')
 
     class Meta:
         ordering = ["-points"]
