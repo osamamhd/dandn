7a1ea46229f84fc13eb545b68031aac04691f45c
Osama Mhd <osamamhd82@gmail.com>
2021-03-08 17:49:56 +0200

    urls path for create story.

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from main.views import StoryListView
+from main.views import StoryListView, StoryCreateView
 
 urlpatterns = [
     path('', StoryListView.as_view(), name='story_list'),
+    path('create/', StoryCreateView.as_view(), name='story_create'),
 ]
