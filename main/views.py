e917242dfb7d530cc546f240fdee98c1451333cf
Osama Mhd <osamamhd82@gmail.com>
2021-03-08 17:51:17 +0200

    Story creation view added.

--- main/views.py
+++ main/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
 from main.models import Story
+from django.urls import reverse
 
 class StoryListView(generic.ListView):
     model = Story
     template_name = 'story_list.html' 
+
+class StoryCreateView(generic.CreateView):
+    model = Story
+    fields = ['title', 'body', 'type_of_story']
+    template_name = 'story_create.html'
+
+    def get_success_url(self):
+        return reverse('story_list')
