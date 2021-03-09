ab5c13f8d7870ea0260ddb3e60ad604c2e02b94d
Osama Mhd <osamamhd82@gmail.com>
2021-03-09 07:18:00 +0200

    url path for story detail view.

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from main.views import StoryListView, StoryCreateView
+from main.views import StoryListView, StoryCreateView, StoryDetailView
 
 urlpatterns = [
     path('', StoryListView.as_view(), name='story_list'),
     path('create/', StoryCreateView.as_view(), name='story_create'),
+    path('<slug:slug>/', StoryDetailView.as_view(), name='story_detail'),
 ]
