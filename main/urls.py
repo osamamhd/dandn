dcf9710a74e0b2de05c50ec5dfc94cb9f652d93e
osamamhd <osamamhd82@gmail.com>
2021-06-26 02:23:05 +0200

    ✨ API url for story list <GET - POST>

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from main.views import StoryListView, StoryCreateView, StoryDetailView
-
+from main import views
 urlpatterns = [
-    path('', StoryListView.as_view(), name='story_list'),
-    path('create/', StoryCreateView.as_view(), name='story_create'),
-    path('<slug:slug>/', StoryDetailView.as_view(), name='story_detail'),
+    path('', views.StoryListView.as_view(), name='story_list'),
+    path('create/', views.StoryCreateView.as_view(), name='story_create'),
+    path('<slug:slug>/', views.StoryDetailView.as_view(), name='story_detail'),
+
+    # URLs for endpoints 
+    path('api/list/', views.StoryList.as_view())
 ]
