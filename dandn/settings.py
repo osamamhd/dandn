3a67abbee703160088190f08fbf976e11e69db69
osamamhd <osamamhd82@gmail.com>
2021-06-26 02:21:08 +0200

    ⚡️ rest_framework and cors configs.

--- dandn/settings.py
+++ dandn/settings.py
@@ -44,15 +44,22 @@ INSTALLED_APPS = [
     'frontend',
     'crispy_forms',
     'crispy_tailwind',
+    'rest_framework',
+    'corsheaders',
 
     # local apps
-    'main.apps.MainConfig'
+    'main.apps.MainConfig',
 
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
