2ebd98d4017f23bf5196eba6e403b4f2ce24233a
osamamhd <osamamhd82@gmail.com>
2021-06-26 02:23:18 +0200

    ✨ API view for story list <GET - POST>

--- main/views.py
+++ main/views.py
@@ -3,6 +3,14 @@ from django.views import generic
 from main.models import Story
 from django.urls import reverse
 
+# imports for api endpoints
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import StorySerializer
+from django.http import Http404
+from rest_framework import status
+
+
 class StoryListView(generic.ListView):
     model = Story
     template_name = 'story_list.html' 
@@ -18,3 +26,21 @@ class StoryCreateView(generic.CreateView):
 class  StoryDetailView(generic.DetailView):
     model = Story
     template_name = 'story_detail.html'
+
+
+"""
+    GET  ===>>> 'stories/api/list'
+    POST ===>>> 'stories/api/list'
+"""
+class StoryList(APIView):
+    def get(self, request, format=None):
+        stories = Story.objects.all()
+        serilizer = StorySerializer(stories, many=True)
+        return Response(serilizer.data)
+
+    def post(self, request, format=None):
+        serializer = StorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
